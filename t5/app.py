c92c7dd4be5a296586a37641ebda9d29d730c9d8
Abba856 <eesabba856@gmail.com>
2025-12-29 15:52:48 +0100

    t5 and django

---
+++ t5/app.py
@@ -0,0 +1,11 @@
+# install django
+# pip install django
+
+# to create a new django project
+# django-admin startproject --name of the project--
+
+#to create django app
+#django-admin startapp --name of the project--
+
+#to run the run django project
+# python manage.py runserver
